871e91f0d5945e62d3e2a1d34688d2b9422fc58b
Marco <mbarozzi@gmail.com>
2025-01-10 16:06:46 -0300

    carrossel best-sellers

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,63 +2,69 @@ import { HomeCard } from '@/components/shared/home/home-card'
 import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import ProductSlider from '@/components/shared/product/product-slider'
 import { Card, CardContent } from '@/components/ui/card'
-import { getAllCategories,getProductsByTag, getProductsForCard } from '@/lib/actions/product.actions'
+import {
+  getAllCategories,
+  getProductsByTag,
+  getProductsForCard
+} from '@/lib/actions/product.actions'
 import { toSlug } from '@/lib/utils'
 
 import data from '@/lib/data'
 
-export default async function HomePage() {
+export default async function HomePage () {
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
   const categories = (await getAllCategories()).slice(0, 4)
   const newArrivals = await getProductsForCard({
     tag: 'new-arrival',
-    limit: 4,
+    limit: 4
   })
   const featureds = await getProductsForCard({
     tag: 'featured',
-    limit: 4,
+    limit: 4
   })
   const bestSellers = await getProductsForCard({
     tag: 'best-seller',
-    limit: 4,
+    limit: 4
   })
+
   const cards = [
     {
       title: 'Categories to explore',
       link: {
         text: 'See More',
-        href: '/search',
+        href: '/search'
       },
-      items: categories.map((category) => ({
+      items: categories.map(category => ({
         name: category,
         image: `/images/${toSlug(category)}.jpg`,
-        href: `/search?category=${category}`,
-      })),
+        href: `/search?category=${category}`
+      }))
     },
     {
       title: 'Explore New Arrivals',
       items: newArrivals,
       link: {
         text: 'View All',
-        href: '/search?tag=new-arrival',
-      },
+        href: '/search?tag=new-arrival'
+      }
     },
     {
       title: 'Discover Best Sellers',
       items: bestSellers,
       link: {
         text: 'View All',
-        href: '/search?tag=new-arrival',
-      },
+        href: '/search?tag=new-arrival'
+      }
     },
     {
       title: 'Featured Products',
       items: featureds,
       link: {
         text: 'Shop Now',
-        href: '/search?tag=new-arrival',
-      },
-    },
+        href: '/search?tag=new-arrival'
+      }
+    }
   ]
 
   return (
@@ -69,6 +75,16 @@ export default async function HomePage() {
         <Card className='w-full rounded-none'>
           <CardContent className='p-4 items-center gap-3'>
             <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title='Best Selling Products'
+              products={bestSellingProducts}
+              hideDetails
+            />
           </CardContent>
         </Card>
       </div>
